a7f53f0b8deee891c30e625e4ae55a2d248c27e7
gonzaalf1689 <gonzaalf1689@gmail.com>
2023-02-17 14:43:21 +0000

    finished code but not rendering onto website

--- src/js/index.js
+++ src/js/index.js
@@ -10,31 +10,33 @@ import "../styles/index.css";
 function SimpleCounter(_props){
     return(<div className="bigCounter">
         <div className="calendar"><i className="fa-regular fa-clock"></i></div>
-        <div className="four">{_props.digitFour}</div>
-        <div className="three">{_props.digitThree}</div>
-        <div className="two">{_props.digitTwo}</div>
-        <div className="one">{_props.digitOne}</div>
+        <div className="four">{_props.digitFour % 10}</div>
+        <div className="three">{_props.digitThree % 10}</div>
+        <div className="two">{_props.digitTwo % 10}</div>
+        <div className="one">{_props.digitOne % 10}</div>
     </div>);
 }
 
 SimpleCounter.propTypes = {
-    digitFour: PropTypes.nuber,
-    digitThree: PropTypes.nuber,
-    digitTwo: PropTypes.nuber,
-    digitOne: PropTypes.nuber,
-}
+    digitFour: propTypes.number,
+    digitThree: propTypes.number,
+    digitTwo: propTypes.number,
+    digitOne: propTypes.number
+};
 
 let counter = 0;
 setInterval(function(){
-    const four = Math.floor(counter/10000);
-    const three = Math.floor(counter/1000);
-    const two = Math.floor(counter/100);
-    const one = Math.floor(counter/10);
-    console.log(four, three, two, one);   
-}, 1000);
-//render your react application
+    const four = Math.floor(counter/1000);
+    const three = Math.floor(counter/100);
+    const two = Math.floor(counter/10);
+    const one = Math.floor(counter/1);
+    console.log(four, three, two, one); 
+    counter ++;  
+
+    //render your react application
 ReactDOM.render(
-    <SimpleCounter digitOne={counter.indexOf()}/>, 
+    <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />, 
     document.querySelector("#app")
 );
+}, 1000);
 
